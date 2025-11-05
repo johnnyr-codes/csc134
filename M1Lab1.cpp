8434903ac744ad3cede0da8c27df3095f14284e5
johnnyr-codes <rodriguj9095@student.faytechcc.edu>
2025-11-05 07:14:11 +0000

    Debug round 2 and completion

--- M1Lab1.cpp
+++ M1Lab1.cpp
@@ -13,15 +13,23 @@ int main() {
 // This program will simulate a storefront for Pokemon GBA flash carts.
 // Variable set up
 string name = "Johnny";
+string item_name = "PKMN flash carts";
 int number_of_carts = 100; // number of carts owned
 double pricePerCart = 0.25; // price per cart
+double total_cost;
 
-// calculate the total price of the carts
-// TODO
+// Greet the customer ^_^
+cout << "Welcome to " << name << "'s Retro Pokecarts Store!" << endl;
 
-// print all the information about the storefront
-// TODO
-cout << "Welcome to " << name;
-cout << " 's Retro PokeCarts." << endl;
+// Ask for an order
+// Assume customer wants to buy EVERYTHING!
+cout << "We have " << number_of_carts << " " << item_name << " in stock. " << endl;
+cout << "Each cart costs $" << pricePerCart << ". " << endl;
 
+// Calculate the total price of all carts
+total_cost = number_of_carts * pricePerCart;
+
+// Output results and thank the customer (pls buy! :D)
+cout << "To purchase all " << number_of_carts << " carts in stock, it would cost $" << total_cost << " total. " << endl;
+cout << "Thank you for shopping with us!" << endl;
 }
