92b369ffd226f7afaedfcdcad5a3ddc715d38574
johnnyr-codes <rodriguj9095@student.faytechcc.edu>
2025-11-05 06:31:35 +0000

    added comment block

--- M1Lab1.cpp
+++ M1Lab1.cpp
@@ -1,6 +1,29 @@
 /*
 CSC 134
 M1Lab1 - Something Sales
-
+johnnyr
+9/10/25
 
 */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+// This program will simulate a storefront for Pokemon GBA flash carts.
+// The owner's name
+string name = "Johnny"
+// number of carts owned
+int carts = 100
+// price per cart
+double pricePerCart = 0.25
+
+// calculate the total price of the carts
+// TODO
+
+// print all the information about the storefront
+// TODO
+cout << "Welcome to " << name;
+cout << " 's Retro PokeCarts." << endl;
+
+}
