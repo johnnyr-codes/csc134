336df5c16fe7cbece6bcca59a0ef5d8b1b69c4a2
johnnyr-codes <rodriguj9095@student.faytechcc.edu>
2025-11-06 10:08:29 +0000

    started M2T1, debug round 1

---
+++ M2T1.cpp
@@ -0,0 +1,34 @@
+/*
+CSC 134
+M2T1 - Customer Service!
+johnnyr
+11/6/25
+*/
+
+ // This program simulates a customer service interaction at a retro gaming store.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+    // Variable set up
+    string first_name, last_name, full_name; // for customer's name
+    string product_name = "flash carts"; // product being sold (cheap doesn't always mean bad for flash carts! Top shelf ones have more features)
+    double cost_each = 0.25;
+    double total_cost;
+
+    // Greet the customer ^_^
+    cout << "Welcome to Johnny's " << product_name << " store!" << endl;
+
+
+
+    // Ask customer how much they want to buy
+
+    // Calculate the price
+    // Give the result
+
+
+    return 0;
+
+}
