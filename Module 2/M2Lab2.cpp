aaab1fd84af3adabe2f239bddc1c891135a98757
johnnyr-codes <rodriguj9095@student.faytechcc.edu>
2025-11-06 16:24:42 +0000

    started M2Lab2

--- Module 2/M2Lab2.cpp
+++ Module 2/M2Lab2.cpp
@@ -6,12 +6,45 @@ johnnyr
 */
 
 #include <iostream>
+#include <iomanip> // for numerical formatting
+using namespace std;
 
 int main() {
-    
+    // Variable set up
+    double width;  // dimensions of the crate
+    double length;
+    double height;
 
+    // Ask user for dimensions
+    cout << "Welcome to the crate volume-cost-profit calculator 9000. Please enter information accurately to ensure accurate results." << endl;
+    cout << "Enter the width: ";
+    cin >> width;
+    cout << "Enter the length: ";
+    cin >> length;
+    cout << "Enter the height: ";
+    cin >> height;
 
+    // Calculate volume
+    double volume = width * length * height;  // volume formula
 
+    cout << "_____________________" << endl; // blank space
+    cout << "Volume =" << "\t" << volume << "cubic units." << endl;
+
+    // Calculate cost
+    double cost_per_cubic_unit = 0.23;
+    double total_cost = volume * cost_per_cubic_unit;
+
+    cout << "Cost =" << "\t$" << total_cost << endl;
+
+    // Calculate customer's charge
+    double customer_charge = volume * 0.5;
+
+    cout << "Customer's charge =" << "\t$" << customer_charge << endl;
+
+    // Calculate profit
+    double profit = customer_charge - total_cost;
+
+    cout << "Profit =" << "\t$" << profit << endl;
     
     return 0;
 }
