5f7e31337ab0b161a4c7593a0f4b16d58602805f
johnnyr-codes <rodriguj9095@student.faytechcc.edu>
2025-11-07 08:27:37 +0000

    added super cool ASCII

--- Module 3/M3T2.cpp
+++ Module 3/M3T2.cpp
@@ -6,11 +6,19 @@ johnnyr
 */
 
 #include <iostream>
+#include <cmath> // for random numbers
+#include <ctime>
 using namespace std;
 
-int main() {[
-
+int main() {
+    // Greet the user
+    cout << "***************************************" << endl; // super awesome asterisk ASCII art
+    cout << "*  Welcome to the Craps Simulator V1! *" << endl;
+    cout << "***************************************" << endl;
+    cout << " " << endl; // blank line for spacing
+    cout << "Roll a 7 or 11, you win!" << endl;  // tell the user the win/lose conditions
+    cout << "Roll a 2 or 12, you lose!" << endl;
 
 
     return 0;
-]
+}
