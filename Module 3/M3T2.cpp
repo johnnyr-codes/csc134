75877a9be772529fb0b69379088eb6007f97c552
johnnyr-codes <rodriguj9095@student.faytechcc.edu>
2025-11-07 08:36:49 +0000

    Instant wins/losses complete

--- Module 3/M3T2.cpp
+++ Module 3/M3T2.cpp
@@ -17,8 +17,52 @@ int main() {
     cout << "***************************************" << endl;
     cout << " " << endl; // blank line for spacing
     cout << "Roll a 7 or 11, you win!" << endl;  // tell the user the win/lose conditions
-    cout << "Roll a 2 or 12, you lose!" << endl;
+    cout << "Roll a 2, 3, or 12, you lose!" << endl;
+    cout << " " << endl; // blank line for spacing
+    cout << " " << endl; // blank line for spacing
 
+    cout << "Time to roll the dice!" << endl;
+    cout << "..." << endl; // ellipses for spacing
+    int seed = time(0);
+
+    //cout << "Your seed is: " << seed << endl;
+    //cout << "What's your lucky number? ";
+    //cin >> seed;
+    // Seed the RNG
+    srand(seed);
+
+    const int MAX = 6; // numbers from 1-6
+    int roll1, roll2, total;
+    roll1  = (rand() % MAX) + 1; // Divide by MAX, and just keep the remainder
+    cout << "First dice: " << roll1 << endl;
+
+    roll2  = (rand() % MAX) + 1; // Divide by MAX, and just keep the remainder
+    cout << "Second dice: " << roll2 << endl;
+
+    total = roll1 + roll2;
+    cout << "..." << endl; // ellipses for spacing
+    cout << "You rolled: " << total << endl;
+
+
+     if (total == 7) {
+        cout << "Lucky seven! You win!" << endl;
+    }
+    else if (total == 11) {
+        cout << "Eleven is a winner!" << endl;
+    }
+    else if (total == 2) {
+        cout << "Snake eyes! Too bad, you lose." << endl;
+    }
+    else if (total == 3) {
+        cout << "Sorry, three is unlucky, you lose." << endl;
+    }
+    else if (total == 12) {
+        cout << "Boxcars! Sorry, you lost." << endl;
+    }
+    else {
+        // anything else
+        cout << "Your point count is now " << total << endl;
+    }
 
     return 0;
 }
